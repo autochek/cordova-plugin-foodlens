ad94d2dce50d7d8a997e56d4c286ec0dac2e7143
cochlear.ai <cochlear.ai@gmail.com>
2019-10-17 18:12:37 +0900

    first commit

---
+++ src/android/Foodlens.java
@@ -0,0 +1,87 @@
+package cordova.plugin.foodlens;
+
+import android.content.Intent;
+import android.util.Log;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import com.doinglab.foodlens.sdk.FoodLens;
+import com.doinglab.foodlens.sdk.UIService;
+import com.doinglab.foodlens.sdk.UIServiceResultHandler;
+import com.doinglab.foodlens.sdk.errors.BaseError;
+import com.doinglab.foodlens.sdk.network.model.UserSelectedResult;
+
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class Foodlens extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("coolMethod")) {
+            String message = args.getString(0);
+            this.coolMethod(message, callbackContext);
+            return true;
+        }
+        if (action.equals("launchFoodlensUI")) {
+            this.launchFoodlensUI(callbackContext);
+            return true;
+        }
+
+        return false;
+    }
+
+    private void coolMethod(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            callbackContext.success("Echo from Coolmethod : "+message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+
+    private UIService uis;
+    private void launchFoodlensUI(CallbackContext callbackContext){
+        this.cordova.setActivityResultCallback (this);
+        uis = FoodLens.createUIService(this.cordova.getActivity().getApplicationContext());
+        uis.startFoodLensCamera(this.cordova.getActivity(), new UIServiceResultHandler() {
+            @Override
+            public void onSuccess(UserSelectedResult result) {
+                try{
+                    callbackContext.success(new JSONObject(result.toJSONString()));
+                }catch(JSONException e){
+                    Log.e("FoodlensPlugin", e.toString());
+                    callbackContext.error(e.toString());
+                }
+            }
+
+            @Override
+            public void onCancel() {
+                //Log.d("MSG_LOG", "Recognition Cancel");
+                callbackContext.error("Cancel");
+            }
+
+            @Override
+            public void onError(BaseError error) {
+                //Log.d("MSG_LOG", error.getMessage());
+                callbackContext.error(error.getMessage());
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) { //65281 for foodlens
+
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if(requestCode==65281)
+            this.uis.onActivityResult(requestCode, resultCode, intent);
+    }
+}
